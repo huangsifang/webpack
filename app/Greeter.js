447775a4c051be6fddffb51e218bd56292be479d
huangsifang <17816869682@163.com>
2017-07-12 13:06:01 +0800

    babel

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,7 +1,23 @@
-// Greeter.js
-var config = require('./config.json');
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
+// loader
+// var config = require('./config.json');
+// module.exports = function() {
+//   var greet = document.createElement('div');
+//   greet.textContent = config.greetText;
+//   return greet;
+// };
+
+// babel
+import React, {Component} from 'react'
+import config from './config.json';
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div>
+        {config.greetText}
+      </div>
+    );
+  }
+}
+
+export default Greeter
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
 	      {
 	        test: /\.json$/,
 	        loader: "json-loader"
+	      },
+	      {
+	        test: /\.js$/,
+	        exclude: /node_modules/,
+	        loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+	        query: {
+	          presets: ['es2015','react']
+	        }
 	      }
 	    ]
   	},
